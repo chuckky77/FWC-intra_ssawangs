030974eee0081f599da13a443cfdad13c790b6eb
Chuckky77 <Chuckmcgill113@gmail.com>
2026-09-18 00:31:37 +0700

    add cell03, ex00

---
+++ cell03/ex00/background.js
@@ -0,0 +1,6 @@
+function changeBackgroundColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    document.body.style.backgroundColor = randomColor;
+}
+
+document.getElementById('bgBtn').addEventListener('click', changeBackgroundColor);
